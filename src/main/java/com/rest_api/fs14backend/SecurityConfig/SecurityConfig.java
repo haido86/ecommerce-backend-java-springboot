97ebb5b1f262084f2a1bf36a7acdf2c11ebcd9e8
haido86 <thithuhai.do@gmail.com>
2023-05-08 10:41:25 +0300

    Backend: whitelist some routes to ignore the login, log out for now

--- src/main/java/com/rest_api/fs14backend/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/rest_api/fs14backend/SecurityConfig/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/signup", "/signin","/products")
+                .requestMatchers("/api/v1/auth/signup", "/api/v1/auth/signin","/api/v1/products","/api/v1/users")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
