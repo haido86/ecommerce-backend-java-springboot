b05d91a840a075a3a57df14c8666a82c410c9de4
haido86 <thithuhai.do@gmail.com>
2023-05-08 10:39:41 +0300

    Backend: add cors config

---
+++ src/main/java/com/rest_api/fs14backend/corsConfig/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.rest_api.fs14backend.corsConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
+
