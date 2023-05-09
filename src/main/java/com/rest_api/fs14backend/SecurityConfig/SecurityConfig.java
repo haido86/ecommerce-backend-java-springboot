82b3ec5e4795de988e772089893ef2fc0dcca305
haido86 <thithuhai.do@gmail.com>
2023-05-09 23:53:42 +0300

    Backend: whitelist the routes for findProductById

--- src/main/java/com/rest_api/fs14backend/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/rest_api/fs14backend/SecurityConfig/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/signup", "/api/v1/auth/signin","/api/v1/products","/api/v1/users")
+                .requestMatchers("/api/v1/auth/signup", "/api/v1/auth/signin","/api/v1/products","/api/v1/users","/api/v1/products/{id}")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
